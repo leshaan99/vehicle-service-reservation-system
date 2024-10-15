cdad86b16b29d621b93b511c66b8c7c069ba9ae2
Leshan Sanjeewa <143873948+leshaan99@users.noreply.github.com>
2024-10-15 12:21:31 +0530

    Create nav bar

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Navbar.css'; 
+import Home from '../Home/Home';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <a href="/">Vehicle Service</a> {Home}
+      </div>
+      <div className="navbar-links">
+        <a href="/login" className="btn-login">Login</a>
+        <a href="/signup" className="btn-signup">Sign Up</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
