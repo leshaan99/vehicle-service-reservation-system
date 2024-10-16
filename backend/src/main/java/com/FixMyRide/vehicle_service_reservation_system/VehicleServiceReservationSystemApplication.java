afa6f48c4f98450e755205f370bd8913ec79ca5d
Leshan Sanjeewa <143873948+leshaan99@users.noreply.github.com>
2024-10-16 22:39:45 +0530

    Connect to daabase

---
+++ backend/src/main/java/com/FixMyRide/vehicle_service_reservation_system/VehicleServiceReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.FixMyRide.vehicle_service_reservation_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleServiceReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleServiceReservationSystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/FixMyRide/vehicle_service_reservation_system/VehicleServiceReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FixMyRide.vehicle_service_reservation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleServiceReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
