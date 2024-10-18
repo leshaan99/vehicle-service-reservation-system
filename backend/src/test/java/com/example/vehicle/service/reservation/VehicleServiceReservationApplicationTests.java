7935346af2d81112f762db15a7d397415141e586
Leshan Sanjeewa <143873948+leshaan99@users.noreply.github.com>
2024-10-18 16:41:38 +0530

    Configure oidc authentication

---
+++ backend/src/test/java/com/example/vehicle/service/reservation/VehicleServiceReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vehicle.service.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleServiceReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
