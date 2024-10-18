77a6f1592c3b2b596297a6d7b13522ec0f8058b1
Leshan Sanjeewa <143873948+leshaan99@users.noreply.github.com>
2024-10-18 19:31:55 +0530

    Update entity class

--- backend/src/main/java/com/example/vehicle/service/reservation/entity/Reservation.java
+++ backend/src/main/java/com/example/vehicle/service/reservation/entity/Reservation.java
@@ -2,17 +2,20 @@ package com.example.vehicle.service.reservation.entity;
 
 import jakarta.persistence.*;
 
+import java.sql.Date;
+import java.sql.Time;
+
 @Entity
 @Table(name = "reservations")
 public class Reservation {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long bookingId;
+    private int bookingId;
 
     private String username;
-    private String date;
-    private String time;
+    private Date date;
+    private Time time;
     private String location;
     private String vehicleNo;
     private int mileage;
