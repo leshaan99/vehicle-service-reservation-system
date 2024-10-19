1aa8b7aa2be10430a67adaf3d8ba36b0605c2d18
Leshan Sanjeewa <143873948+leshaan99@users.noreply.github.com>
2024-10-19 10:01:16 +0530

    Create security configuration class

---
+++ backend/src/main/java/com/example/vehicle/service/reservation/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.example.vehicle.service.reservation.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/reservations").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .authorizationEndpoint(authorization -> authorization
+                                .baseUri("/oauth2/authorize")
+                        )
+                        .redirectionEndpoint(redirection -> redirection
+                                .baseUri("/login/oauth2/callback/*")
+                        )
+                );
+
+        return http.build();
+    }
+}
+
+
+
